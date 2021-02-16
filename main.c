4c8dd29579b276093dd399b6db8be39723f85151
Jiang Kevin <kevinjiang2313@gmail.com>
2021-02-16 03:01:21 +0000

    finalised

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <math.h>
 
+//note that all units will be in inches
+
 double obtain_x(double a, double b, double h, double c);
 double obtain_y(double a, double b, double h, double c);
 
 int main(void) {
-  double a,b,h,c;
+  double a,b,c,h;
   double xc,yc;
   printf("Please enter the shorter side (tip chord)\n");
   scanf("%lf",&a);
@@ -14,25 +16,33 @@ int main(void) {
   scanf("%lf",&b);
 
   printf("Please enter the difference between the two sides (sweep)\n");
-  scanf("%lf",&h);
-
-  printf("Please enter the distance between the parallel sides (semi-span)\n");
   scanf("%lf",&c);
 
-  xc = obtain_x(a,b,h,c);
-  yc = obtain_y(a,b,h,c);
+  printf("Please enter the distance between the parallel sides (semi-span)\n");
+  scanf("%lf",&h);
 
-  printf("Your centroid is located at (%.3f, %.3f)\n", xc, yc);
+  xc = obtain_x(a,b,c,h);
+  yc = obtain_y(a,b,c,h);
 
+  printf("Your centroid is located at (%.4f, %.4f)\n", xc, yc);
   return 0;
 }
 
-double obtain_x(double a, double b, double h, double c) {
-  double resultx = (2*a*c+a*a+c*b+b*b)/(3*(a+b));
+double obtain_x(double a, double b, double c, double h) {
+  double resultx = (2*a*c+a*a+c*b+a*b+b*b)/(3*(a+b));
   return resultx;
 }
 
-double obtain_y(double a, double b, double h, double c) {
+double obtain_y(double a, double b, double c, double h) {
   double resulty = h*(2*a+b)/(3*(a+b));
   return resulty;
 }
+
+/* Test Case 
+a = 2.25
+b = 12
+c = 9.75
+h = 4
+expected value: (7.88, 1.544)
+obtained value: (7.8816, 1.5439)
+*/
